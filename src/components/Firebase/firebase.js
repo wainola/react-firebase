ced8d50023c71435f55c1a6f96d5b45a3346f682
Nicolás Riquelme <nrriquelme@gmail.com>
2019-04-05 23:56:31 -0300

    sign methods from firebase api

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const {
   REACT_APP_API_KEY,
@@ -21,6 +22,27 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    this.auth = app.auth();
+  }
+
+  doCreateUserWithEmailAndPassword(email, password) {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  doSignInWithEmailAndPassword(email, password) {
+    this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  doSignOut() {
+    this.auth.signOut();
+  }
+
+  doPasswordReset(email) {
+    this.auth.sendPasswordResetEmail(email);
+  }
+
+  doPasswordUpdate(password) {
+    this.auth.currentUser.updatePassword(password);
   }
 }
 
